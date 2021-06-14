f1115e3161dfae2a9993a9a7d3b9c24ed56b6814
Hamid Salehi <salehi.hamidreza@gmail.com>
2021-06-13 17:21:00 -0700

    launch on rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+var mnemonic = "broom light family give heart auto height mistake setup digital fossil illness";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +9,15 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "wss://rinkeby.infura.io/ws/v3/8b0faa13300946fabe9e480c21995e95");
+      },
+      network_id: '4',
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200,
+      addressIndex: 2
     }
   },
   compilers: {
